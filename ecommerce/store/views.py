f5a4bed9744f7a7fbf83f9cefb7ab9cb866dc1c0
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-01-17 11:13:39 +0200

    Navbar

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def store(request):
@@ -13,3 +13,17 @@ def cart(request):
 def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('store')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid login credentials'})
+    else:
+        return render(request, 'login.html')
+
