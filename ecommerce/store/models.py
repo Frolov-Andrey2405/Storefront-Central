a6599b7acbfbdc8eca8e06c3d8e6b28d89e94413
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-01-18 13:51:56 +0200

    cart total`s

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -25,12 +25,25 @@ class Product(models.Model):
 class Order(models.Model):
     '''This class represents an order in the system'''
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
-    date_orderd = models.DateTimeField(auto_now_add=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class OrderItem(models.Model):
     '''This class represents an item in an order in the system'''
