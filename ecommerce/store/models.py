5b035100f233c5bb3f79850c6b6a2f619d35ee81
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-01-18 12:30:12 +0200

    render cart data

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -30,7 +30,7 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.id)
 
 class OrderItem(models.Model):
     '''This class represents an item in an order in the system'''
@@ -38,6 +38,11 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     '''This class represents a shipping address for an order in the system'''
