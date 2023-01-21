0e3bcd3731e67c74179e12153eaa00e4eabe779f
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-01-21 20:03:19 +0200

    cartItems -> checkout, cart

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -34,16 +34,19 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {
             'get_cart_total': 0,
             'get_cart_items': 0
         }
-        
+        cartItems = order['get_cart_items']
+
     context = {
         'items': items,
-        'order': order
+        'order': order,
+        'cartItems': cartItems
         }
     return render(request, 'store/cart.html', context)
 
@@ -54,16 +57,19 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {
             'get_cart_total': 0,
             'get_cart_items': 0
         }
+        cartItems = order['get_cart_items']
 
     context = {
         'items': items,
-        'order': order
+        'order': order,
+        'cartItems': cartItems
         }
 
     return render(request, 'store/checkout.html', context)
