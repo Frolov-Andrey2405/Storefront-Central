b374f9683776591ffab130179d0a0ef3a56a3c3b
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-01-23 14:15:28 +0200

    Process Order

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -101,3 +102,32 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse('Item updated', status=200, safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+
+    else:
+        print('User in not logging in ...')
+
+    return JsonResponse('Payment complete!', status=200, safe=False)
